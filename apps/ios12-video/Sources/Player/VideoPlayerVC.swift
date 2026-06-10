f88bb125d499c5471cfeeab928653b54bfe7513f
djsxhz <33350100@qq.com>
2026-06-10 11:46:43 +0800

    已添加 User-Agent 伪装到 WKWebView
    iOS 16

--- apps/ios12-video/Sources/Player/VideoPlayerVC.swift
+++ apps/ios12-video/Sources/Player/VideoPlayerVC.swift
@@ -73,6 +73,8 @@ final class VideoPlayerVC: UIViewController {
         webView.scrollView.isScrollEnabled = false
         webView.scrollView.contentInsetAdjustmentBehavior = .never
         webView.navigationDelegate = self
+        // Spoof User-Agent to match AppDelegate's setting
+        webView.customUserAgent = "Mozilla/5.0 (iPhone; CPU iPhone OS 16_5 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.5 Mobile/15E148 Safari/604.1"
         view.addSubview(webView)
         installCloseGestures(on: webView)
         self.webView = webView
